07df191583875cffd128cac8bf3d287d26f17356
JEEVANANTHAN A <106263059+jeevananthan27@users.noreply.github.com>
2025-12-10 09:32:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Remove the following block:
-  // esbuild: {
-  //   loader: 'jsx',
-  //   include: /src\/.*\.js$/,
-  // }
 })
